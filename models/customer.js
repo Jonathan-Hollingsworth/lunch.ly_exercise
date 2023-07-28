3753cdc9bafc97d74730fc666d47b9b5540b0c7d
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-07-28 13:22:09 -0400

    Made new methods and description uniform

--- models/customer.js
+++ models/customer.js
@@ -53,11 +53,12 @@ class Customer {
     return new Customer(customer);
   }
 
-
   /** get the full name of this customer */
+
   fullName() {
     return `${this.firstName} ${this.lastName}`
   }
+
   /** get all reservations for this customer. */
 
   async getReservations() {
